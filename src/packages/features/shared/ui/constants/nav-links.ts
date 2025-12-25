5013f43955e76d7bbfa374a8c271495c9710ee65
Oliver Böhm <oliverboehmprivat@gmail.com>
2025-12-25 22:57:08 +0100

    feat: change discover to home

--- src/packages/features/shared/ui/constants/nav-links.ts
+++ src/packages/features/shared/ui/constants/nav-links.ts
@@ -2,35 +2,35 @@ import { Avatar } from "@radix-ui/react-avatar";
 import { BarChart, CookingPot, DollarSign, HomeIcon } from "lucide-react";
 
 export type NavItem = {
-    name: string;
-    href: string;
-    icon: any;
+  name: string;
+  href: string;
+  icon: any;
 };
 
 export const MAIN_NAV_ITEMS: NavItem[] = [
-    {
-        name: "Discover",
-        icon: HomeIcon,
-        href: "/discover",
-    },
-    {
-        name: "Meal Plan",
-        icon: CookingPot,
-        href: "/cooking",
-    },
-    {
-        name: "Budget",
-        icon: DollarSign,
-        href: "/budget",
-    },
-    {
-        name: "Health",
-        icon: BarChart,
-        href: "/nutrition-tracking",
-    },
-    {
-        name: '',
-        icon: <Avatar></Avatar>,
-        href: 'profile'
-    }
+  {
+    name: "Discover",
+    icon: HomeIcon,
+    href: "/",
+  },
+  {
+    name: "Meal Plan",
+    icon: CookingPot,
+    href: "/cooking",
+  },
+  {
+    name: "Budget",
+    icon: DollarSign,
+    href: "/budget",
+  },
+  {
+    name: "Health",
+    icon: BarChart,
+    href: "/nutrition-tracking",
+  },
+  {
+    name: "",
+    icon: <Avatar></Avatar>,
+    href: "profile",
+  },
 ];
