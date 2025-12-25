5873d5d4f6ece62926221e337b2215781dc4b81f
Oliver Böhm <oliverboehmprivat@gmail.com>
2025-12-26 00:57:14 +0100

    refactor: bell

--- src/packages/features/shared/ui/components/top-mobile-header/notification-bell.tsx
+++ src/packages/features/shared/ui/components/top-mobile-header/notification-bell.tsx
@@ -7,9 +7,19 @@ import {
   ItemDescription,
   ItemGroup,
   ItemMedia,
+  ItemSeparator,
   ItemTitle,
 } from "@/components/ui/item";
 import { NotificationMessage } from "../../../api/model/messages";
+import { Button } from "@/components/ui/button";
+import { BUTTON_STYLES } from "./styles/search";
+import { Bell, Check } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Fragment } from "react/jsx-runtime";
 
 function NotificationsList({
   notifications,
@@ -17,35 +27,78 @@ function NotificationsList({
   notifications: NotificationMessage[];
 }) {
   return (
-    <ScrollArea className="h-[320px]">
+    <ScrollArea className="h-[300px]">
       <ItemGroup variant={"muted"}>
         {notifications.map(({ id, actor, title, message, created_at }) => (
-          <Item key={id}>
-            <ItemMedia>
-              <Avatar className="h-8 w-8">
-                {actor.avatar_url ? (
-                  <AvatarImage src={actor.avatar_url} />
-                ) : (
-                  <AvatarFallback>
-                    {actor.name[0]}
-                    {actor.name[1]}
-                  </AvatarFallback>
-                )}
-              </Avatar>
-            </ItemMedia>
+          <Fragment key={id}>
+            <Item>
+              <ItemMedia>
+                <Avatar>
+                  {actor.avatar_url ? (
+                    <AvatarImage src={actor.avatar_url} />
+                  ) : (
+                    <AvatarFallback>
+                      {actor.name[0]}
+                      {actor.name[1]}
+                    </AvatarFallback>
+                  )}
+                </Avatar>
+              </ItemMedia>
 
-            <ItemContent className="flex flex-col">
-              <ItemTitle>{title}</ItemTitle>
-              <ItemDescription className="text-pretty">
-                {message}
-              </ItemDescription>
-              <p>{new Date(created_at).toLocaleDateString()}</p>
-            </ItemContent>
-          </Item>
+              <ItemContent className="flex flex-col">
+                <ItemTitle>{title}</ItemTitle>
+                <ItemDescription className="text-pretty">
+                  {message}
+                </ItemDescription>
+                <p>{new Date(created_at).toLocaleDateString()}</p>
+              </ItemContent>
+            </Item>
+            <ItemSeparator />
+          </Fragment>
         ))}
       </ItemGroup>
     </ScrollArea>
   );
 }
 
-export default NotificationsList;
+function NotificationListHeader({ unread }: { unread: number }) {
+  return (
+    <div className="flex items-center justify-between pb-2 border-b">
+      <h3 className="text-sm font-bold">Notifications</h3>
+      <span className="text-xs text-muted-foreground">{unread} unread</span>
+    </div>
+  );
+}
+
+function NotifictionIndicator({ unread }: { unread: number }) {
+  return (
+    <div className="relative">
+      {unread > 0 && (
+        <div className="absolute -right-1 -top-1 bg-destructive rounded-full text-xs h-3 w-3 flex items-center justify-center text-white"></div>
+      )}
+      <Bell size={64} />
+    </div>
+  );
+}
+
+function NotificationBell() {
+  const { unread, data } = mockNotificationsResponse;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className={BUTTON_STYLES} size="icon-sm" variant="ghost">
+          <NotifictionIndicator unread={unread} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="space-y-2">
+        <NotificationListHeader unread={unread} />
+        <NotificationsList notifications={data} />
+        <Button variant="secondary" className="w-full">
+          Mark all as read
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export { NotificationBell, NotificationsList, NotificationListHeader };
--- src/packages/features/shared/ui/components/top-mobile-header/top-mobile-header.tsx
+++ src/packages/features/shared/ui/components/top-mobile-header/top-mobile-header.tsx
@@ -10,81 +10,20 @@ import {
   ItemMedia,
   ItemTitle,
 } from "@/components/ui/item";
-import { cn } from "@/lib/utils";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import {
-  Bell,
-  ChevronLeftCircle,
-  Newspaper,
-  SearchIcon,
-  Settings,
-  X,
-} from "lucide-react";
+import { ChevronLeftCircle, Newspaper, SearchIcon, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { mockNotificationsResponse } from "../../../api/mocks/messages";
-import NotificationsList from "./notification-bell";
+import {
+  BACKDROP_STYLES,
+  BUTTON_STYLES,
+  POPOVER_STYLES,
+} from "./styles/search";
+import { NotificationBell } from "./notification-bell";
 
 const mockImg = "/essen.jpeg";
 const ingredientThumbailSize = 100;
-
-const SHARED_TRANSITION = "transition-all duration-300 ease-in-out";
-
-const BUTTON_STYLES = cn(
-  SHARED_TRANSITION,
-  "max-w-[50px] opacity-100",
-  "group-focus-within/search:max-w-0 group-focus-within/search:opacity-0"
-);
-
-const POPOVER_STYLES = cn(
-  "bg-secondary p-4 rounded-b-lg shadow-lg w-full absolute left-0 z-50 top-9",
-  SHARED_TRANSITION,
-  "invisible opacity-0",
-  "group-focus-within/search:visible group-focus-within/search:opacity-100"
-);
-
-const BACKDROP_STYLES = cn(
-  "fixed inset-0 z-40 backdrop-blur-xs",
-  SHARED_TRANSITION,
-  "opacity-0 invisible pointer-events-none",
-  "group-focus-within/search:opacity-100 group-focus-within/search:visible group-focus-within/search:pointer-events-auto"
-);
-
-function NotificationBell() {
-  const numberOfNotifications = 5;
-
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button className={BUTTON_STYLES} size="icon-sm" variant="ghost">
-          <div className="relative">
-            <div className="absolute -right-1 -top-1 bg-destructive rounded-full text-[10px] h-3 w-3 flex items-center justify-center text-white">
-              {numberOfNotifications}
-            </div>
-            <Bell />
-          </div>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[360px] p-0">
-        <div className="flex items-center justify-between p-4 border-b">
-          <h3 className="text-sm font-medium">Notifications</h3>
-          <span className="text-xs text-muted-foreground">
-            {mockNotificationsResponse.unread} unread
-          </span>
-        </div>
-
-        <NotificationsList notifications={mockNotificationsResponse.data} />
-      </PopoverContent>
-    </Popover>
-  );
-}
 
 function DiscountsQuickLink() {
   return (
