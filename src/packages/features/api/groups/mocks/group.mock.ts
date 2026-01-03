37977c1f70e0096d1104ae627eb6a0cb441b574e
Oliver Böhm <oliverboehmprivat@gmail.com>
2026-01-03 12:14:55 +0100

    fix: missing values

--- src/packages/features/api/groups/mocks/group.mock.ts
+++ src/packages/features/api/groups/mocks/group.mock.ts
@@ -1,110 +1,87 @@
+import { eachMonthOfInterval } from "date-fns";
+import { ro } from "date-fns/locale";
 import { Home, User } from "lucide-react";
+import { GroupMember } from "../model/group";
 
-const MOCK_USERS = [
-    {
-        id: "1",
-        username: "jdoe",
-        email: "jdoe@mail.com",
-        firstname: "John",
-        lastname: "Doe",
-    },
-    {
-        id: "2",
-        username: "asmith",
-        email: "asmith@mail.com",
-        firstname: "Anna",
-        lastname: "Smith",
-    },
-    {
-        id: "3",
-        username: "bwayne",
-        email: "bwayne@mail.com",
-        firstname: "Bruce",
-        lastname: "Wayne",
-    },
+const MOCK_USERS: GroupMember[] = [
+  {
+    id: "1",
+    username: "jdoe",
+    email: "jdoe@mail.com",
+    firstname: "John",
+    lastname: "Doe",
+    role: "admin",
+    imgUrl: "/ingredient.png",
+  },
+  {
+    id: "2",
+    username: "asmith",
+    email: "asmith@mail.com",
+    firstname: "Anna",
+    role: "member",
+    lastname: "Smith",
+  },
+  {
+    id: "3",
+    username: "bwayne",
+    email: "bwayne@mail.com",
+    firstname: "Bruce",
+    lastname: "Wayne",
+    role: "viewer",
+    imgUrl: "/ingredient.png",
+  },
 ];
 
-const MOCK_OTHER_GROUP_MEMBERS = [
-    {
-        id: "4",
-        username: "ckent",
-        email: "ckent@mail.com",
-        firstname: "Clark",
-        lastname: "Kent",
-    },
-    {
-        id: "5",
-        username: "dprince",
-        email: "dprince@mail.com",
-        firstname: "Diana",
-        lastname: "Prince",
-    },
+const MOCK_OTHER_GROUP_MEMBERS: Omit<GroupMember, "role">[] = [
+  {
+    id: "4",
+    username: "ckent",
+    email: "ckent@mail.com",
+    firstname: "Clark",
+    lastname: "Kent",
+  },
+  {
+    id: "5",
+    username: "dprince",
+    email: "dprince@mail.com",
+    firstname: "Diana",
+    lastname: "Prince",
+  },
 ];
 
 const GROUPS_MOCK = [
-    {
-        id: "123e4567-e89b-12d3-a456-426614174001",
-        name: "At Home",
-        isActive: true,
-        logo: Home,
-        address: {
-            street: "Musterstraße",
-            houseNo: "123",
-            zipCode: "123456",
-            city: "Berlin",
-            country: "Germany",
-        },
-        description:
-            "Eine Gruppe für alle die bei Dir zu Hause wohnen und zusammen einkaufen",
-        members: [
-            {
-                id: "123e4567",
-                firstname: "Oliver",
-                role: "admin",
-                lastname: "Boehm",
-                imgUrl: "/ingredient.png",
-            },
-            {
-                id: "123e4568",
-                firstname: "Nastja",
-                role: "member",
-                lastname: "Marsov",
-            },
-            {
-                id: "123e4569",
-                firstname: "Liam",
-                role: "guest",
-                lastname: "Marsov",
-                imgUrl: "/essen.jpeg",
-            },
-        ],
+  {
+    id: "123e4567-e89b-12d3-a456-426614174001",
+    name: "At Home",
+    isActive: true,
+    logo: Home,
+    address: {
+      street: "Musterstraße",
+      houseNo: "123",
+      zipCode: "123456",
+      city: "Berlin",
+      country: "Germany",
     },
-    {
-        id: "123e4567-e89b-12d3-a456-426614174000",
-        name: "For Me",
-        isActive: false,
-        logo: User, 
-        address: {
-            street: "Berlepschstraße",
-            houseNo: "123",
-            zipCode: "123456",
-            city: "Berlin",
-            country: "Germany",
-        },
-        description:
-            "Ein Gruppe nur für Dich, falls Du mal etwas nicht mit anderen teilen möchtest.",
-        members: [
-            {
-                id: "123e4567",
-                firstname: "Oliver",
-                lastname: "Boehm",
-                role: "admin",
-                imgUrl: "/ingredient.png",
-            },
-        ],
+    description:
+      "Eine Gruppe für alle die bei Dir zu Hause wohnen und zusammen einkaufen",
+    members: [...MOCK_USERS],
+  },
+  {
+    id: "123e4567-e89b-12d3-a456-426614174000",
+    name: "For Me",
+    isActive: false,
+    logo: User,
+    address: {
+      street: "Berlepschstraße",
+      houseNo: "123",
+      zipCode: "123456",
+      city: "Berlin",
+      country: "Germany",
     },
+    description:
+      "Ein Gruppe nur für Dich, falls Du mal etwas nicht mit anderen teilen möchtest.",
+    members: [...MOCK_USERS.slice(0, 1)],
+  },
 ];
 
-export {
-    GROUPS_MOCK, MOCK_USERS, MOCK_OTHER_GROUP_MEMBERS
-}
+export { GROUPS_MOCK, MOCK_USERS, MOCK_OTHER_GROUP_MEMBERS };
--- src/packages/features/api/groups/model/group.ts
+++ src/packages/features/api/groups/model/group.ts
@@ -1,11 +1,19 @@
-export type GroupMemberRole = 'admin' | 'member' | 'viewer'
+export type GroupMemberRole = "admin" | "member" | "viewer";
 
-export interface GroupMember  {
+export interface GroupMember {
   id: string;
   username: string;
   email: string;
   firstname: string;
   lastname: string;
-  role: GroupMemberRole
+  role: GroupMemberRole;
   imgUrl?: string | null;
-};
+}
+
+export interface User {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  username: string;
+}
